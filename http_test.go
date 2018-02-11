9e6e6cd3d4e3369ce2156e6d2f7b1a5a359f9a60
whao <mobairuby@gmail.com>
2018-02-11 12:11:19 +0800

    mock the test file

---
+++ http_test.go
@@ -0,0 +1,14 @@
+package httputils
+
+import (
+	"log"
+	"testing"
+)
+
+func TestGetHttpPool(t *testing.T) {
+	log.Println("this is a test")
+}
+
+func TestHttpConPool_Request(t *testing.T) {
+	log.Println("this is another test")
+}
